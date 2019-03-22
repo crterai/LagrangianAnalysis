afb6d0dec21c143fe00af93d0fba8870da6ff810
crterai <rterai@uci.edu>
2019-03-21 22:00:54 -0700

    Updates to scripts and for LTS

--- test_run.py
+++ test_run.py
@@ -25,9 +25,9 @@ model_prefix='longcam5I_L30_20081001_00Z_f09_g16_1024'
 derived_output_location='/global/cscratch1/sd/terai/UP_analysis/Eastman_analysis/CAM5_1deg/'
 year='2009'
 #months=['01','02','03','04','05','06','07','08','09']
-months=['09']
-datestr=['30','31']
-#datestr=['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
+months=['10','11','12']
+#datestr=['30','31']
+datestr=['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
 timestr=['00000','21600','43200','64800']
 
 for i in months: #np.arange(4):
@@ -43,25 +43,25 @@ for i in months: #np.arange(4):
             except:
                 print ''.join(['Tried for',mo_date,'-',date,'-',time,': moving on'])
                 continue
-            U=f_in1('U')
-            V=f_in1('V')
+            T=f_in1('T')
+            RELHUM=f_in1('RELHUM')
             p0=f_in1('P0')
             a=f_in1('hyam')
             b=f_in1('hybm')
             ps=f_in1('PS')
             P=create_variables.create_P(ps,a,b,p0)
-            U,V=create_variables.EIS_LTS(U,V,P,925)
-            outfile=''.join([derived_output_location,'Winds_',model_prefix,'.cam.h1.',year,'-',mo_date,'-',date,'-',time,'.nc'])
+            LTS,EIS=create_variables.EIS_LTS(T,P,RELHUM)
+            outfile=''.join([derived_output_location,'LTS_',model_prefix,'.cam.h1.',year,'-',mo_date,'-',date,'-',time,'.nc'])
             f_out=cdm.open(outfile,'w')
             
-            f_out.write(U)
-            f_out.write(V)
+            f_out.write(LTS)
+            #f_out.write(V)
             f_out=create_netcdfs.transfer_attributes(f_in1,f_out)
             f_out=create_netcdfs.globalAttWrite(f_out) ; # Use function to write standard global atts to output file
             setattr(f_out,'script_URL','https://github.com/crterai/Analysis/commit/')
             f_out=create_netcdfs.add_git_hash(f_out)
-            filename=os.path.basename(__file__)
-            setattr(f_out,'script_used',filename)
+            #filename=os.path.basename(__file__)
+            #setattr(f_out,'script_used',filename)
 
             f_in1.close()
             f_out.close()
